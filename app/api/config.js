52b8f4a8af4c162ac1de215f3926966cbbe03504
bigclawBin <117381340+bigclawBin@users.noreply.github.com>
2024-05-29 20:15:04 +0300

    all

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-back.nomorepartiesco.ru'
+export const BASE_URL = 'https://pindie-back.nomorepartiesco.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
